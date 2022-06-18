e95b0bdb78e8807a3fd03ee4b68495bd651ce21c
Mandevd <99964027+Mandevd@users.noreply.github.com>
2022-06-18 00:13:08 -0500

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -1,3 +1,17 @@
+/**
+using single stack: In this we will be using single stack and what we will be doing is: 
+we will be pushing every element and along with that if the new element's value is less
+than the previous minimum we will be pushing the previous minimum along with the new value 
+and will update our minimum value to the new element's value. and while popping if the popped 
+value is equal to the minimum value at that point we will pop 2 values one for the element to
+be popped and the other one is going to be the value which will be used to update the minimum
+variable. and with top and getmin function we can just get the topmost value and the minimum 
+variable's value respectively for both the functions.
+*/
+
+
+
+
 import java.util.Stack;
 
 public class MinStack {
